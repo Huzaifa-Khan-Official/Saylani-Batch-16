79e6c7d96d2be227de66716f5da829291a93e601
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-06-21 15:10:57 +0500

    JavaScript 1st Class

---
+++ JavaScript/Class 1/app.js
@@ -0,0 +1,14 @@
+// alert('Hello World!')
+
+// :root {
+// --abc: black;
+// }
+
+
+var acb1 = "Hello, Welcome to Karachi!"
+var abc2 = 456789
+var abc3 = 2.5
+var abc4 = true
+
+// alert(typeof(abc4))
+alert(abc4)
