ec2305f183c5f1e138c812fd46c2b63d9b5d3103
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-06-29 14:26:45 +0500

    class 3

---
+++ JavaScript/Class 3/app.js
@@ -0,0 +1,116 @@
+// var num; // varaibale declaration
+// var num = 1 // variable initialization
+
+// document.write("Value after variable declaration is: ??")
+// variable declaration me variable ki value undefined rehti hai
+
+// num = 30
+
+// document.write("</br>Initial value: " + num) // 30
+
+// num++ // post increment // 30
+
+                                            // num = num + 1
+// document.write("</br>Value after increment is: " + num)
+
+// num = num + 7
+
+// document.write("</br>Value after addition is: " + num)
+
+// num--
+
+// document.write("</br>Value after decrement is: " + num)
+
+// num = num % 3
+
+// document.write("</br>The remainder is : " + num)
+
+
+// var num = 6
+// var num2 = 6
+
+// var res = num % num2
+// document.write(res)
+
+// var res = 3 - 2 * 4 * (3 + 7) + 5;
+// 3 - 2 * 4 * 10 + 5
+// 3 - 80 + 5
+// 3 - 75
+// -72
+// var res = ((2 * 4) * 4) + 2;
+        //(8 * 4) + 2
+        // 32 + 2 // 34
+
+// var res = (2 * 4) * (4 + 2)
+//       //  8 * 6
+
+// document.write(res)
+
+// Conditional Operatiors
+
+// < less than
+// > greater than
+// >= greater or equal
+// <= less or equal
+
+// == left ko right se match karna
+// === left aur right ko match karna hai with data type
+// && And Operator, left side ke sath sath right side ki condition true hogi tu hi aage jane donga
+// || Or operator, koi bhi aik side agar ture hoi, tu aage jane donga
+
+
+
+// if (8 === 8) {
+//   document.write('Han, sahi hai!')
+// } else {
+//   document.write('Ghalat kardiya')
+// }
+
+// var email = "example@gmail.com"
+// var password = "example@gmail.com"
+
+// if (email === password) {
+//   document.write("Login Success")
+// } else {
+//   alert("Invalid email or password")
+// }
+
+
+// var email = prompt("Enter your email:")
+// var password = prompt("Enter your password:")
+
+// if (email == "admin" || password == "123456") {
+//   alert("Login successful")
+// } else {
+//   alert("email or password pehle sahi likho")
+// }
+
+// var per = prompt("Enter your percenatage: ");
+
+// if (per >= 80 && per <= 100) {
+//   document.write("Grade: A+")
+// } else if (per >=70 && per < 80) {
+//   document.write("Grade: A")
+// } else if (per >=60 && per < 70) {
+//   document.write("Grade: B")
+// } else if (per >=50 && per < 60) {
+//   document.write("Grade: C")
+// } else if (per >=40 && per < 50) {
+//   document.write("Grade: D")
+// } else if (per >=30 && per < 40) {
+//   document.write("Grade: E")
+// } else if (per > 0 && per < 30) {
+//   document.write("Fail, mar khane ke liye tayar hojao")
+// } else {
+//   document.write("Invalid percentage")
+// }
+
+
+// Marksheet
+// Roll No.
+// Marks of 5 subjects
+// total = 500
+// obtain = sub1 + sub2 + sub3 + sub4 + sub5
+// percentage = obtain / total * 100
+// grade
+// Marksheet
