c5c36ca8480ad4627df00e4fb8764641dae47f66
Huzaifa-Khan-Official <mhuzaifakhan2004@gmail.com>
2025-07-19 13:24:52 +0500

    class 6

---
+++ JavaScript/Class 6/app.js
@@ -0,0 +1,69 @@
+
+
+// var arr = [1, 3, 5, 6, 7]
+
+// indexes ==> 4
+// length ==> 5
+//  i < length  ==> 4 < 5
+
+// document.write(arr)
+// document.write(arr[0] + " " + arr[1])
+
+// for(var i = 0; i < arr.length ; i++) {
+//   document.write(arr[i] + " ") // 1 3 5 6 7 undefined
+// }
+
+// var arr = ["karachi", "lahore", "islamabad", "peshawar", "quetta"]
+// var userInp = prompt("Enter city name: ")
+// var isMatch = false
+// var isFound = "no"
+
+// for (var i = 0; i < arr.length; i++) { // i = 0, 1
+  // if (userInp.toUpperCase() == arr[i].toUpperCase()) { // quetta == "karachi", quetta == "lahore"
+    // isMatch = true
+    // isFound = "yes"
+    // alert("city found")
+    // break
+    // document.write(arr[i] + " ")
+//   }
+// } 
+
+// if (isMatch == false) {
+//   alert("city not found")
+// }
+
+// if (isFound == "no") {
+//   alert("city not found")
+// }
+
+// if (!isMatch) {
+//   alert("city not found")
+// }
+
+// else {
+//  alert("city not found")
+// }
+
+
+
+// if (userInp == arr) { // karachi == ""karachi", "lahore", "islamabad", "peshawar", "quetta"]"
+//   document.write("City found")
+// } else {
+//   document.write("city not found")
+// }
+
+
+
+// var arr = ["Salman", "Sohail", "Arbaaz"]
+// var arr2 = ["Saleem Khan", "Sami Bhai", "Hadi", "Rehman"]
+
+// for (var i = 0; i < arr.length; i++) {
+
+//   for (var j = 0; j < arr2.length; j++) {
+//     document.write(arr[i] + " " + arr2[j] + "</br>")
+//   }
+
+
+//   // document.write(arr[i] + " " + arr2[0] + "</br>")
+//   // document.write(arr[i] + " " + arr2[1] + "</br>")
+// }
