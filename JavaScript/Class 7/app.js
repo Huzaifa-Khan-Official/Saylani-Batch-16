59e25a2bb5f345512120988d5204c8ce12ddf455
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-07-20 12:34:48 +0500

    Class 7

---
+++ JavaScript/Class 7/app.js
@@ -0,0 +1,58 @@
+// var arr = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"]
+// var userInp = prompt("Enter city name: ")
+// var firstChar = userInp.slice(0, 1).toUpperCase()
+// var remainChar = userInp.slice(1).toLowerCase()
+// var res = firstChar + remainChar
+// var isFound = false
+
+// var city = "kaRacHi"
+// var firstChar = city.slice(0, 1).toUpperCase()
+// var remainChar = city.slice(1).toLowerCase()
+// console.log(firstChar + remainChar);
+
+// for(var i = 0; i < arr.length; i++) {
+//   if (res === arr[i]) {
+//     isFound = true
+//     alert("City found")
+//     break
+//   }
+// }
+
+// if (!isFound) {
+//   alert("City not found!")
+// }
+
+
+
+// for(var i = 0; i < 10; i++) { // 0, 1
+//   for(var j = 1; j <= 10; j++) { // 1
+//     var res = i * 10 + j // res = 11
+//     document.write(res + " ") 
+//     // 1 2 3 4 5 6 7 8 9 10
+//     // 11
+//   }
+//   document.write("</br>")
+// }
+
+// document.write("</br>")
+
+
+// for (var i = 1; i <= 100; i = i + 10) { // 1 11 21 31 41 ... 91
+//     for(var j = i; j < i + 10; j++) { // j < 11 + 10
+//       document.write(j + " ")
+//     }
+
+//     document.write("</br>")
+//   // document.write(i + "</br>")
+// }
+
+// var arr = ["Zain", "Shahzaib", "Maaz", "Saim", "Zain", "Shahzaib", "Maaz", "Zain"]
+
+// document.write(arr.indexOf("Suleman"))
+// document.write(arr.lastIndexOf("Zain"))
+
+// document.write(arr.)
+
+var cityName = "Karachi"
+
+document.write(cityName.charAt(3))
