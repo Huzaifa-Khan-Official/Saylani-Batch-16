90a674a653606d2bd7a3c422c677bdb02b116bda
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-08-16 14:36:11 +0500

    Class 11, Class 12

---
+++ JavaScript/Class 11/app.js
@@ -0,0 +1,61 @@
+// var num = 3 + (4 - 5) * (2 / 2);
+          // 3 + (-1) * 1
+          // 3 + (-1)
+          // 3 - 1
+          // 2
+// var num = (1 + 3) * 4
+          // 4 * 4 ==> 16 
+
+// var num = ((2 * 4) * 4) + 2
+          // (8 * 4) + 2
+          // 32 + 2
+          // 34, 
+// console.log("num ==>", num);
+
+// var expression1 = 4 > 4 // false
+// var expression2 = "" // true
+//                 // 
+// console.log("expression2 ==>", expression2);
+
+// 0
+// NaN
+// undefinied
+// null
+// empty string
+// false
+
+// if (expression2) {
+//   console.log("in the console section");
+// }
+
+
+// var num1 = 123
+// var num2 = "123"
+
+// if (expression1 !== expression2) { // false != 5 == 5 // false
+//   console.log("in the console section");
+// }
+
+
+// for (var i = 0; i < 20; i++) {
+//   console.log("Hello, World! ==>", i + 1);
+// }
+
+// for (var i = 20; i > 0; i--) {
+//   console.log("Hello, World! ==>", i);
+// }
+
+// var cities = ["Karachi", "Islamabad", "Korangi", "Orangi", "Landi"]
+
+
+// var para = document.getElementById("para").innerHTML
+
+
+
+
+function getValue() {
+  var inp = document.getElementById("userName").value
+  console.log("inp ==>", inp);
+}
+
+// console.log("para ==>", para);
