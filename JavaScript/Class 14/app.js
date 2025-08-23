c7d05cff4dd960e072be3b7e9444afb39c6762b5
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-08-23 14:40:47 +0500

    class 14

---
+++ JavaScript/Class 14/app.js
@@ -0,0 +1,88 @@
+// var dayOfWeek = "Thur"
+
+// switch (dayOfWeek) {
+//   case "Sat":
+//     console.log("Whoopee");
+//     break
+//   case "Sun":
+//     console.log("Whoopee");
+//     break
+
+//   case "Fri":
+//     console.log("TGIF");
+//     break
+//   default:
+//     console.log("Shoot me now!");
+// }
+
+
+// While Loops
+
+// var num = 0
+
+// while (num < 20) {
+//   console.log("num ==>", num);
+  
+//   num++
+// }
+
+
+// var num = 10;
+// var isTrue = true;
+
+// while (isTrue) { // true, true, true, true, true, false
+//   if (num == 6) { // 10,  9, 8, 7, 6
+//     isTrue = false;
+//   }
+
+//   console.log("in the loop"); // 1, 2, 3, 4, 5
+//   num--;
+// }
+
+
+/*
+var num = 10;
+var isTrue = true;
+
+do {
+  if (num == 6) { // 10, 9, 8, 7, 6
+    isTrue = false;
+  }
+  
+  console.log("in the loop"); // 1, 2, 3, 4, 5
+  num--;
+} while (isTrue)
+*/
+console.log("out of loop")
+
+
+// var num = 10;
+// var isTrue = false;
+
+// do {
+//   if (num == 6) { // 10
+//     isTrue = false;
+//   }
+  
+//   console.log("in the loop"); // 1
+//   num--;
+// } while (isTrue); // flase
+
+
+// if (false) {
+
+// }
+
+// var num = 0
+
+// while (num < 0) {
+//   console.log("in the loop");
+//   num++
+// }
+
+// var num = 0
+
+// do {
+//   console.log("in the loop");
+//   num++;
+// } while (num < 0)
