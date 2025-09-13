8ff86af72d6da7c51be47a88e785bae47d846fe2
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-09-13 14:01:30 +0500

    todo application complete

--- JavaScript/Class 17/app.js
+++ JavaScript/Class 17/app.js
@@ -40,14 +40,23 @@
 var input = document.getElementById("input");
 
 function addTodo() {
-  var ul = document.getElementById("output");
-  var li = document.createElement("li");
-  var counter = ul.childElementCount;
+  if (input.value == "") {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cancel'
+    })
+  } else {
+    var ul = document.getElementById("output");
+    var li = document.createElement("li");
+    var counter = ul.childElementCount;
 
-  li.innerHTML = "<div class='outputLi'>" + input.value + "<button onclick='deleteTodo(" + counter + ")'>Delete</button></div>"
-  li.setAttribute("id", counter)
-  ul.appendChild(li)
-  input.value = ""
+    li.innerHTML = "<div class='outputLi'>" + input.value + "<div><button onclick='editTodo(" + counter + ")'>Edit</button><button onclick='deleteTodo(" + counter + ")'>Delete</button></div></div>"
+    li.setAttribute("id", counter)
+    ul.appendChild(li)
+    input.value = ""
+  }
 }
 
 function deleteTodo(id) {
@@ -55,11 +64,34 @@ function deleteTodo(id) {
   li.remove();
 }
 
-// edit button, with id pass
-// editBtn, get li with id
-// get text of that li, and set the input value with the li text
-// hide the add button, show the edit btn,
-// add a function in the attribue onclick in edit btn
-// make a function editTodo
-// get the same li, set the value with new value;
+// edit button, with id pass (Done)
+// editBtn, get li with id (Done)
+// get text of that li, and set the input value with the li text (Done)
+// hide the add button, show the edit btn, (Done)
+// add a function in the attribue onclick in edit btn (Done)
+// make a function editTodoLi (Done)
+// get the same li, set the value with new value; (Done)
 // hide the edit btn, show the add btn.
+
+var editLiId;
+function editTodo(id) {
+  var li = document.getElementById(id);
+  // console.log("li ==>", li.childNodes[0].childNodes[0]);
+  // console.log("text ==>", li.firstChild.firstChild.nodeValue);
+  
+  // input.value = li.childNodes[0].childNodes[0].nodeValue
+  var text = li.firstChild.firstChild.nodeValue;
+  input.value = text;
+  document.getElementById("addBtn").style.display = "none";
+  document.getElementById("editBtn").style.display = "inline-block";
+  // editLiId = id;
+}
+
+function editTodoLi() {
+  console.log("input ==>", input.value);
+  var li = document.getElementById(editLiId);
+  li.firstChild.firstChild.nodeValue = input.value;
+  input.value = ""
+  document.getElementById("addBtn").style.display = "inline-block";
+  document.getElementById("editBtn").style.display = "none";
+}
