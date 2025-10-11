64fe193a5eb91ee47c435aae722bb87494c70dab
Huzaifa-Khan-Official <m.huzaifakhan2004@gmail.com>
2025-10-11 14:30:20 +0500

    Class 25

---
+++ JavaScript/Class 25/public/app.js
@@ -0,0 +1,55 @@
+
+
+// function buldOnOFf() {
+//   var btn = document.getElementById("btn");
+//   var imgTag = document.getElementById("imgTag");
+
+//   if (btn.innerText == "Bulb On") {
+//     imgTag.src = "./assets/bulbon.webp";
+//     btn.innerText = "Bulb Off"
+//   } else {
+//     imgTag.src = "./assets/bulboff.jpg";
+//     btn.innerText = "Bulb On"
+//   }
+
+//   console.log("imgSrc ==>", imgTag.src);
+// }
+
+// console.log("p ==>", document.getElementsByTagName("p"));
+
+// console.log("p ==>", document.getElementsByClassName("para"));
+
+// var paras = document.getElementsByTagName("p")
+// console.log("paras ==>", paras);
+
+// var para = paras[0].getElementsByTagName("p");
+// var div =  paras[0].getElementsByTagName("p")
+// console.log("div ==>", div);
+
+
+// var para = document.getElementById("para");
+// console.log("para ==>", para);
+
+// SAAS
+// firebase == BAAS
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAs4RrWT_UDkZFVKHpl5AswahBGPEesJWw",
+  authDomain: "saylani-b-16.firebaseapp.com",
+  projectId: "saylani-b-16",
+  storageBucket: "saylani-b-16.firebasestorage.app",
+  messagingSenderId: "778162040871",
+  appId: "1:778162040871:web:77a97c0b622d7481c29e02"
+};
+
+// Initialize Firebase
+var app = initializeApp(firebaseConfig);
+
+
