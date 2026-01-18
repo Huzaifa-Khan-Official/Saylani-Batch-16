261d56938094fa840e06f295bc2ccee38d65ad9f
Meer Quais <meerquais017@gmail.com>
2026-01-18 15:01:04 +0500

    react started

---
+++ React/Class 1/vite-react/src/App.jsx
@@ -0,0 +1,11 @@
+// Component: A component is a part of UI which can be reusebale, It is a simple/arrow function which returns an HTML element. A component name must start with a capital letter.
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, react project</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt voluptatem placeat modi, eaque cum, sequi beatae cumque blanditiis ea odit ut sunt numquam veritatis autem. Animi alias repellat atque voluptatibus!</p>
+    </div>
+  )
+}
